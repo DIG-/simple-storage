3445e976c4f1367164e06ebbdeb46a0a39ec447a
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-05-11 13:59:49 -0300

    Changed encoder of StorageKeyHashSHA256

--- core/src/main/java/br/dev/dig/storage/core/common/hash/StorageKeyHashSHA256.kt
+++ core/src/main/java/br/dev/dig/storage/core/common/hash/StorageKeyHashSHA256.kt
@@ -1,17 +1,16 @@
 package br.dev.dig.storage.core.common.hash
 
 import br.dev.dig.storage.core.operation.KeyHash
+import br.dev.dig.storage.core.operation.encodian.Encode
 import java.security.MessageDigest
-import java.util.*
 
-class StorageKeyHashSHA256 : KeyHash {
+class StorageKeyHashSHA256(private val encoder: Encode) : KeyHash {
 
     private val digest = MessageDigest.getInstance("SHA-256")
-    private val encoder = Base64.getEncoder()
 
     override fun hash(key: String): String {
         val hash = digest.digest(key.toByteArray())
-        return encoder.encodeToString(hash)
+        return encoder.encode(hash)
     }
 
 }
