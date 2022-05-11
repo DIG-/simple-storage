7c31e33f63e5a49e3062dd6d0e45ff9757583c39
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-05-11 09:23:38 -0300

    Sample project init

---
+++ sample/src/main/java/br/dev/dig/storage/sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.dev.dig.storage.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.dev.dig.storage.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
