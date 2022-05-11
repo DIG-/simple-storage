9b8db9a834d9590af3b3036e708d2d5727ea612a
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-05-11 13:30:33 -0300

    Created InternalStorage

---
+++ core/src/main/java/br/dev/dig/storage/core/exception/KeyNotFound.kt
@@ -0,0 +1,4 @@
+package br.dev.dig.storage.core.exception
+
+class KeyNotFound(message: String? = null, cause: Throwable? = null) :
+    RuntimeException(message, cause)
---
+++ core/src/main/java/br/dev/dig/storage/core/operation/InternalStorage.kt
@@ -0,0 +1,11 @@
+package br.dev.dig.storage.core.operation
+
+import br.dev.dig.storage.core.exception.KeyNotFound
+
+interface InternalStorage {
+    @Throws(KeyNotFound::class)
+    fun get(key: String): String
+    fun put(key: String, value: String)
+    fun contains(key: String): Boolean
+    fun delete(key: String)
+}
