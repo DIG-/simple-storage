4632d80d7db60cef56218a0e4959be5f9d229d94
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-05-11 15:06:04 -0300

    Added Gson serializer

---
+++ serializer/gson/src/main/java/br/dev/dig/storage/serializer/GsonSerializer.kt
@@ -0,0 +1,17 @@
+package br.dev.dig.storage.serializer
+
+import br.dev.dig.storage.core.common.serialization.StorageSerializationBasic
+import com.google.gson.Gson
+
+class GsonSerializer @JvmOverloads constructor(private val gson: Gson = Gson()) :
+    StorageSerializationBasic() {
+
+    override fun <T : Any> toJson(item: T): String {
+        return gson.toJson(item)
+    }
+
+    override fun <T : Any> fromJson(json: String, clazz: Class<T>): T {
+        return gson.fromJson(json, clazz)
+    }
+
+}
